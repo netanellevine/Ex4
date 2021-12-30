13342308ad368fd05be0f7f79d434501dfb6d56b
Netanel <netanellevine123@gmail.com>
2021-12-30 18:27:43 +0200

    working on building a graph

--- main.c
+++ main.c
@@ -5,10 +5,16 @@
 
 
 
-void freeGraph(ptrNode head);
+void freeGraph(ptrNode *head);
+void freeEdges(ptrNode *node);
 void printGraph_cmd(ptrNode head);
 void addEdge(ptrNode *head, int source, int dest, int weight);
+void addNode(ptrNode *head, int id);
 void initGraph(ptrNode *head, int size);
+ptrNode getNode(ptrNode head, int id);
+bool contains(ptrNode head, int id);
+
+ptrNode lastNode;
 
 int main() {
     printf("START!\n");
@@ -16,6 +22,7 @@ int main() {
     int DONE = 0;
     int amount_of_nodes = 0;
     ptrNode ptrGraph;
+//    ptrNode lastNode;
     while (DONE != EOF) {
         DONE = scanf("%c", &input);
         switch (input) {
@@ -48,10 +55,34 @@ int main() {
             case 'E': {
                 break;
             }
-//            case 'B':
-//            {
-//
-//            }
+            case 'B':
+            {
+                int new_id;
+                scanf("%d", &new_id);
+                if(!contains(ptrGraph, new_id)){
+                    addNode(&ptrGraph, new_id);
+                }
+                else{
+                    ptrNode curr_n = getNode(ptrGraph, new_id);
+                    freeEdges(&curr_n);
+                }
+                int dest = -1;
+                int weight = -1;
+                int num = -1;
+                while (scanf("%d", &num)) {
+                    if (new_id != -1 && dest == -1 && weight == -1) {
+                        dest = num;
+                    } else if (new_id != -1 && dest != -1 && weight == -1) {
+                        weight = num;
+                    }
+                    if (new_id != -1 && dest != -1 && weight != -1) {
+                        addEdge(&ptrGraph, new_id, dest, weight);
+                        dest = -1;
+                        weight = -1;
+                    }
+                }
+                break;
+            }
 //            case 'D':
 //            {
 //
@@ -67,7 +98,7 @@ int main() {
         }
     }
 //    printGraph_cmd(arr);
-    freeGraph(ptrGraph);
+    freeGraph(&ptrGraph);
     return 0;
 }
 
@@ -95,6 +126,7 @@ void initGraph(ptrNode *head, int size){
             last = node;
         }
     }
+    lastNode = last;
 }
 
 
@@ -170,12 +202,24 @@ void printGraph_cmd(ptrNode head) {
 }
 
 
-void freeGraph(ptrNode head) {
+bool contains(ptrNode head, int id){
+    ptrNode curr_n = head;
+    while(curr_n != NULL){
+        if(curr_n->node_num == id){
+            return true;
+        }
+        curr_n = curr_n->next;
+    }
+    return false;
+}
+
+
+void freeGraph(ptrNode *head) {
     if (head == NULL) {
         return;
     }
     ptrNode curr_n;
-    curr_n = head;
+    curr_n = *head;
     while (curr_n != NULL) {
         ptrNode next_node = curr_n->next;
         ptrEdge curr_e = curr_n->edges;
@@ -187,4 +231,43 @@ void freeGraph(ptrNode head) {
         free(curr_n);
         curr_n = next_node;
     }
+}
+
+
+void freeEdges(ptrNode *node) {
+    if (node == NULL) {
+        return;
+    }
+    ptrEdge curr_e = (*node)->edges;
+    while (curr_e != NULL) {
+        ptrEdge next_edge = curr_e->next;
+        free(curr_e);
+        curr_e = next_edge;
+    }
+}
+
+
+void addNode(ptrNode *head, int id){
+    ptrNode newNode;
+    newNode = (ptrNode) malloc(sizeof(Node));
+    if (newNode == NULL) {
+        printf("Memory didn't allocated!\n");
+        exit(0);
+    }
+    newNode->node_num = id;
+    newNode->next = NULL;
+    newNode->edges = NULL;
+    newNode->edges_s = 0;
+    lastNode->next = newNode;
+    lastNode = newNode;
+}
+
+
+ptrNode getNode(ptrNode head, int id){
+    ptrNode curr_n;
+    curr_n = head;
+    while(curr_n->node_num != id){
+        curr_n = curr_n->next;
+    }
+    return curr_n;
 }
